c25afa1333f23eb84b7edb95640ba13aff6edb50
yashwardhan17 <yashwardhan.1717@gmail.com>
2026-05-01 15:32:33 +0530

    update api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://team-task-manager-production-80fa.up.railway.app/api',
 });
 
 API.interceptors.request.use((config) => {
